77d6056ffa1b6e037bcc98967b16d2495521ff30
toneyparky <qkrtmddhks95@gmail.com>
2020-03-02 19:32:13 +0900

    feat: GameNumber 생성자 예외처리

--- src/main/java/baseball/domain/GameNumber.java
+++ src/main/java/baseball/domain/GameNumber.java
@@ -9,9 +9,19 @@ package baseball.domain;
  * 날짜 : 2020/03/02
  */
 public class GameNumber {
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 9;
+
 	private final int gameNumber;
 
 	public GameNumber(final int inputGameNumber) {
+		validate(inputGameNumber);
 		this.gameNumber = inputGameNumber;
+	}
+
+	private void validate(final int inputGameNumber) {
+		if (inputGameNumber < MIN_NUMBER || inputGameNumber > MAX_NUMBER) {
+			throw new IllegalArgumentException("GameNumber의 범위를 초과한 입력이 있습니다.");
+		}
 	}
 }
--- src/test/java/baseball/domain/GameNumberTest.java
+++ src/test/java/baseball/domain/GameNumberTest.java
@@ -2,7 +2,7 @@ package baseball.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 /**
@@ -18,5 +18,19 @@ public class GameNumberTest {
 	@Test
 	void GameNumber_생성자_올바른_동작_확인() {
 		assertThat(new GameNumber(1)).isInstanceOf(GameNumber.class);
+	}
+
+	@Test
+	void GameNumber_생성자_허용_범위_미만_입력시_예외처리() {
+		assertThatThrownBy(() -> new GameNumber(0))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("GameNumber의 범위를 초과한 입력이 있습니다.");
+	}
+
+	@Test
+	void GameNumber_생성자_허용_범위_초과_입력시_예외처리() {
+		assertThatThrownBy(() -> new GameNumber(10))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("GameNumber의 범위를 초과한 입력이 있습니다.");
 	}
 }
