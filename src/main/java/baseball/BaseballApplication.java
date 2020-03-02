061ea3c2d09b1e34e2c76b17e26db2452297bf69
toneyparky <qkrtmddhks95@gmail.com>
2020-03-02 19:24:36 +0900

    feat: GameNumber 생성자 동작 확인

---
+++ src/main/java/baseball/BaseballApplication.java
@@ -0,0 +1,15 @@
+package baseball;
+
+/**
+ * 클래스 이름 : .java
+ *
+ * @author
+ * @version 1.0
+ * <p>
+ * 날짜 : 2020/03/02
+ */
+public class BaseballApplication {
+	public static void main(String[] args) {
+
+	}
+}
