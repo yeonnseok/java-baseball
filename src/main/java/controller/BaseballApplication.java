a1afbcee065968406a4f0488f4f877381945f40c
yeonnseok <ljinsk3@gmail.com>
2020-03-02 23:13:41 +0900

    refactor: baseballGame 과 main controller로 통합

--- src/main/java/controller/BaseballApplication.java
+++ src/main/java/controller/BaseballApplication.java
@@ -1,16 +1,37 @@
-import controller.BaseballGame;
-import domain.Flag;
+package controller;
+
+import domain.*;
 import view.InputView;
 import view.OutputView;
 
-public class Main {
+public class BaseballApplication {
 
     public static void main(String[] args) {
-        BaseballGame baseBallGame = new BaseballGame();
         Flag restartFlag = Flag.RESTART;
         while (restartFlag != Flag.EXIT) {
-            baseBallGame.play();
+            ComputerNo computerNo = new ComputerNo(new RandomBallNumberGenerator());
+            boolean isCorrect = false;
+            repeatUntilAnswerCorrect(isCorrect, computerNo);
+            OutputView.printCorrectBallsMessage();
             restartFlag = inputRestartFlagWithValidation();
+        }
+    }
+
+    private static void repeatUntilAnswerCorrect(boolean isCorrect, ComputerNo computerNo) {
+        while (!isCorrect) {
+            UserNo userNo = createUserWithInputValidation();
+            CountResult countResult = new CountResult(computerNo, userNo);
+            OutputView.printCountResultMessage(countResult);
+            isCorrect = countResult.isThreeStrike();
+        }
+    }
+
+    private static UserNo createUserWithInputValidation() {
+        try {
+            return new UserNo(new ManualBallNumberGenerator(InputView.inputUserNumber()));
+        } catch (IllegalArgumentException | NullPointerException e) {
+            OutputView.printExceptionMessage(e);
+            return createUserWithInputValidation();
         }
     }
 
@@ -40,4 +61,5 @@ public class Main {
             throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
     }
+
 }
