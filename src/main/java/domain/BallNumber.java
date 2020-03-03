1ee30363a465edf117db98f4095fa80e5038e594
yeonnseok <ljinsk3@gmail.com>
2020-03-03 18:24:20 +0900

    refactor: equals convention 정리

--- src/main/java/domain/BallNumber.java
+++ src/main/java/domain/BallNumber.java
@@ -20,8 +20,12 @@ public class BallNumber {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         BallNumber that = (BallNumber) o;
         return number == that.number;
     }
