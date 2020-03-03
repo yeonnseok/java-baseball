cb0eedc80fb31d0b20a6e501aaa8e3e865b68a08
giantim <giantim132@gmail.com>
2020-03-03 13:31:46 +0900

    refactor: 테스트 코드 의미 구체화

--- src/test/java/domain/BaseballNumberTest.java
+++ src/test/java/domain/BaseballNumberTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class BaseballNumberTest {
-    @DisplayName("Baseball 생성자에 유효한 값이 전달되었을 때 예외 발생 테스트")
+    @DisplayName("Baseball 생성자에 유효하지 않은 값이 전달되었을 때 예외 발생 테스트")
     @ParameterizedTest
     @ValueSource(ints = {0, -1, 10})
     void baseballConstructorTest(int input) {
